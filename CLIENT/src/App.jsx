df520d007dba4b7432b6ed9f4ac033f381ca2291
radicalHighway <igor.papin178@gmail.com>
2025-04-03 09:52:35 +0300

    release CSS OnePage

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -4,32 +4,30 @@ import Layout from "./components/widgets/Layout";
 import LoginPage from "./components/page/LoginPage";
 import SignInPage from "./components/page/SigninPage";
 import { useEffect, useState } from "react";
-import axiosInstance, {
-  setAccessToken,
-} from "./components/shared/lib/axiosInstance";import OneRecieptCard from "./components/widgets/OneRecieptCard"
-
+import axiosInstance, {setAccessToken} from "./components/shared/lib/axiosInstance";
+import OneRecieptCard from "./components/widgets/OneRecieptCard"
 import FavoritesPage from "./components/page/FavoritesPage";
 
 function App() {
-  const [user, setUser] = useState({ status: "logging", data: null });
+  const [user, setUser] = useState({ status: 'logging', data: null });
   const handleLogout = () => {
     axiosInstance
-      .get("/auth/logout")
-      .then(() => setUser({ status: "guest", data: null }));
-    setAccessToken("");
+      .get('/auth/logout')
+      .then(() => setUser({ status: 'guest', data: null }));
+    setAccessToken('');
   };
 
   useEffect(() => {
-    axiosInstance("/tokens/refresh")
+    axiosInstance('/tokens/refresh')
       .then(({ data }) => {
         setTimeout(() => {
-          setUser({ status: "logged", data: data.user });
+          setUser({ status: 'logged', data: data.user });
         }, 1000);
         setAccessToken(data.accessToken);
       })
       .catch(() => {
-        setUser({ status: "guest", data: null });
-        setAccessToken("");
+        setUser({ status: 'guest', data: null });
+        setAccessToken('');
       });
   }, []);
   return (
